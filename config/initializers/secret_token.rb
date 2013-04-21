340c67b93402b9465b1f5ff1b5ad4e1bf18200cc
Pierre <pierre@sugaryourcoffee.de>
2013-04-21 20:22:54 +0200

    Initial commit of secondhand

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secondhand::Application.config.secret_token = '41b9006a393934af7229ffd3eb2aa1f3705197495d51a54579a0f7d108d9045e98b342a059d82ccd8cf4bc6391bd18b4ea706dda36f951ed03e22d7d1154d4bd'
