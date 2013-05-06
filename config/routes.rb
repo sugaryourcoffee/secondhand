9b7ed0ad845ad52ff7c9741385d8ac47cf7e1b86
Pierre <pierre@sugaryourcoffee.de>
2013-05-06 23:13:05 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Secondhand::Application.routes.draw do
   root to: 'static_pages#home'
 
+  match '/signup',  to: 'users#new'
+
   match '/about',   to: 'static_pages#about'
   match '/help',    to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
-
-  get "users/new"
 
   resources :lists
   resources :users
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,6 @@ describe "User pages" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: 'Sign up') }
+    it { should have_title('Sign up') }
   end
 end
