e90011de83082479b754ca59205cb9bef7b61e9c
Pierre <pierre@sugaryourcoffee.de>
2013-05-06 22:23:24 +0200

    Add links from header and footer to help, about, home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,48 +6,48 @@ describe "Static pages" do
 
   describe "Home page" do
     it "should have the h1 'Secondhand'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Secondhand')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_title("#{base_title} | Home")
     end
   end
 
   describe "Help page" do
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_title("#{base_title} | Contact")
     end
   end
