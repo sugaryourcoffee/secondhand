4c68438e7d2c75ca008d1395c7e9b06a13bfe862
Pierre <pierre@sugaryourcoffee.de>
2013-05-11 12:16:38 +0200

    Restrict edit profile only to the user itself

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,6 +46,22 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "E-Mail", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_title('Edit user')
+          end
+        end
+      end
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -56,6 +72,24 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+      end
+
+      describe "as wrong user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:wrong_user) do 
+          FactoryGirl.create(:user, email: "wrong@example.com")
+        end
+        before { sign_in user }
+
+        describe "visiting Users#edit page" do
+          before { visit edit_user_path(wrong_user) }
+          it { should_not have_title(full_title('Edit user')) }
+        end
+
+        describe "submitting a PUT request to the Users#update action" do
+          before { put user_path(wrong_user) }
+          specify { response.should redirect_to(root_path) }
         end
       end
     end
