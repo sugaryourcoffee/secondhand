372daee623662df0ae9b25613fa19b61fcc9c1cd
Pierre <pierre@sugaryourcoffee.de>
2013-06-02 23:23:24 +0200

    Create list as pdf for print out. Start with header, footer and items

--- app/models/list.rb
+++ app/models/list.rb
@@ -21,16 +21,43 @@ class List < ActiveRecord::Base
                   :user_id
 
   def list_pdf
+
+    header_left   = event.title
+    header_center = "www.boerse-burgthann.de"
+    header_right  = "Liste #{self.list_number}"
+
     items_list = items.map do |item|
       [item.item_number, item.description, item.size, item.price]
     end
 
     pdf = Prawn::Document.new
-    pdf.table([[ "Number", "Description", "Size", "Price"], *items_list]) do |t|
+    pdf.move_down 10
+    pdf.table([[ "No", "Description", "Size", "Price"], 
+               *items_list], column_widths: [30, 350, 80, 80]) do |t|
       t.header = true
+      t.columns(0).align = :right
+      t.columns(1).align = :left
+      t.columns(2).align = :right
+      t.columns(3).align = :right
       t.row(0).style(background_color: '44844', text_color: 'ffffff')
-      t.columns(1).align = :right
+      t.row(0).font_style = :bold
+      t.row(0).columns(0..3).align = :center
     end
+
+    pdf.repeat(:all) do
+      pdf.draw_text header_left, at: pdf.bounds.top_left
+      pdf.draw_text header_center, at: [pdf.bounds.left+200,pdf.bounds.top]
+      pdf.draw_text header_right, at: pdf.bounds.top_right
+    end
+
+    pdf.repeat(:all, dynamic: true) do
+      pdf.number_pages "<page>/<total>",
+                       { start_count_at: 1,
+                         at: [pdf.bounds.right - 50, 0],
+                         align: :right,
+                         size: 14 }
+    end
+
     pdf.render
   end
 
