1ef176f9269c8dd8ce6fa3700295af62dc9c3e7b
Pierre <pierre@sugaryourcoffee.de>
2013-06-19 20:50:04 +0200

    Finalize locale switcher in _header.html.erb and add current_language helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def selected_language
+    unavailable = lambda { "Language" }
+    LANGUAGES.detect(unavailable) { |l| l[1] == I18n.locale.to_s }[0]
+  end
+
 end
