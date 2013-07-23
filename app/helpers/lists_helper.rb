1bb4645163ff3d77a4077e94c763027c49285bd1
Pierre <pierre@sugaryourcoffee.de>
2013-07-23 20:53:59 +0200

    Show event title and user name in lists index and lists show page

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,3 +1,21 @@
 module ListsHelper
 
+  def event_title_for(list)
+    event = Event.find_by_id(list.event_id)
+    if event
+      event.title
+    else
+      list.event_id
+    end
+  end
+  
+  def user_for(list)
+    user = User.find_by_id(list.user_id)
+    if user
+      link_to "#{user.last_name}, #{user.first_name}", user
+    else
+      list.user_id
+    end
+  end
+
 end
