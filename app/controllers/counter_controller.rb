819770a7934bf1ded75125024180bc0cb2e24797
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-06-13 16:55:13 +0200

    add reversals for counter index view

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -3,6 +3,7 @@ class CounterController < ApplicationController
   def index
     @carts = Cart.not_empty
     @sellings = Selling.latest_on_top
+    @reversals = Reversal.latest_on_top
   end
 
 end
