03c9ac30a7b8fcc57205505d56aeb5f9fdf7da01
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-06-14 14:50:07 +0200

    supplement create_selling by item_count

--- spec/support/reversal_support.rb
+++ spec/support/reversal_support.rb
@@ -1,6 +1,6 @@
 def create_reversal(event, selling = nil, offset = 0, line_item_count = 1)
   reversal = Reversal.new(event_id: event.id)
-  selling  = create_selling(event) unless selling
+  selling  = create_selling(event, line_item_count) unless selling
 
   raise "invalid line item count" if offset + line_item_count > selling.
                                                             line_items.size 
