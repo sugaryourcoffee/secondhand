4584348b8ea8d13bdd20ae343be98ac1f71a01db
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-06-14 14:45:43 +0200

    add filter for finding specific selling or reversal

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,9 +1,26 @@
 class CounterController < ApplicationController
 
   def index
-    @carts = Cart.not_empty
-    @sellings = Selling.latest_on_top
-    @reversals = Reversal.latest_on_top
+    @carts     = Cart.not_empty
+    @sellings  = retrieve_sellings(params)
+    @reversals = retrieve_reversals(params)
   end
 
+  private
+
+    def retrieve_sellings(params)
+      if params[:selling_id] and not params[:selling_id].empty?
+        Selling.where(id: params[:selling_id])
+      else
+        Selling.latest_on_top
+      end
+    end
+
+    def retrieve_reversals(params)
+      if params[:reversal_id] and not params[:reversal_id].empty?
+        Reversal.where(id: params[:reversal_id])
+      else
+        Reversal.latest_on_top
+      end
+    end
 end
