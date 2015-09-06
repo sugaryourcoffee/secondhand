c745bf71163681adaa042c8ed0fec56234a3a1f3
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-09-06 16:33:39 +0200

    add spec for role authentication

---
+++ spec/requests/role_authentication_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'Role authentication' do
+  
+  describe 'as regualar user' do
+  end
+
+  describe 'as operator user' do
+  end
+
+  describe 'as admin user' do
+  end
+
+end
+
