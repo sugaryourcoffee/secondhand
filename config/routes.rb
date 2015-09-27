8601537d04f8c4f6005c3cc3496ecc12f13bb0c8
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-09-27 23:27:52 +0200

    change match to rails 4 conform actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Secondhand::Application.routes.draw do
 
   scope '(:locale)' do
-    root to: 'static_pages#home'
+    root                'static_pages#home' # root to: 'static_pages#home'
 
-    match '/signup',  to: 'users#new'
-    match '/signin',  to: 'sessions#new'
-    match '/signout', to: 'sessions#destroy', via: :delete
+    get    'signup'  => 'users#new'         # match '/signup',  
+                                            # to: 'users#new'
+    get    'signin'  => 'session#new'       # match '/signin',  
+                                            # to: 'sessions#new'
+    delete 'signout' => 'session#destroy'   # match '/signout', 
+                                            # to: 'sessions#destroy', 
+                                            # via: :delete
 
-    match '/about',   to: 'static_pages#about'
-    match '/help',    to: 'static_pages#help'
-    match '/contact', to: 'static_pages#contact'
-    match '/message', to: 'static_pages#message'
+    get    'about'   => 'static_pages#about' # match '/about',   
+                                             # to: 'static_pages#about'
+    get    'help'    => 'static_pages#help'  # match '/help',    
+                                             # to: 'static_pages#help'
+    get    'contact' => 'static_pages#contact' # match '/contact', 
+                                               # to: 'static_pages#contact'
+    get    'message' => 'static_pages#message' # match '/message', 
+                                               # to: 'static_pages#message'
 
     get   '/who_registered' => 'users#who_registered', 
                                defaults: { format: 'atom' }
@@ -100,60 +108,4 @@ Secondhand::Application.routes.draw do
     end
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
