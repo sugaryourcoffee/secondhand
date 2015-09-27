e8d6fe1475c018e83c0c024d3fad93a3b2abdb24
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-09-27 23:28:45 +0200

    changed configuration files with rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Secondhand::Application.config.secret_token = '41b9006a393934af7229ffd3eb2aa1f3705197495d51a54579a0f7d108d9045e98b342a059d82ccd8cf4bc6391bd18b4ea706dda36f951ed03e22d7d1154d4bd'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Secondhand::Application.config.secret_key_base = '3a01c8d112282cec247a46b3c036dd2845f7b3c53a8e5c52311d9589b4b052a917b9384e8bbe785f2f3c2a01495858a184a8bb1ff088bea247d6542cde2b7782'
