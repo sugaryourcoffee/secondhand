7e6fef909ec56a2a8511642c05d3ca612277827a
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-10-11 21:54:06 +0200

    remove obsolete commented out attr_accessibles

--- app/models/selling.rb
+++ app/models/selling.rb
@@ -7,8 +7,6 @@ class Selling < ActiveRecord::Base
   has_many :line_items
   has_many :reversals, through: :line_items
   
-#  attr_accessible :event_id
-
   before_destroy :ensure_line_items_empty
 
   scope :latest_on_top, lambda { order("created_at desc") }
@@ -16,18 +14,7 @@ class Selling < ActiveRecord::Base
   def total
     line_items.inject(0) { |sum, line_item| sum + (line_item.reversal ? 0 : line_item.price) } 
   end
-=begin
-  def revenue
-    line_items.inject(0) { |sum, line_item| sum + line_item.price }
-  end
 
-  def add_items_from_cart(cart)
-    cart.line_items.each do |line_item|
-      line_item.cart_id = nil
-      line_items << line_item
-    end 
-  end
-=end
   private
 
     def ensure_line_items_empty
