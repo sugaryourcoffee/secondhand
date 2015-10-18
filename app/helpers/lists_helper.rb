6e728ddcb4c144245b91f90c9476a908ae2e9132
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-10-18 21:28:54 +0200

    remove link to user show page in acceptances index page

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -9,10 +9,14 @@ module ListsHelper
     end
   end
   
-  def user_for(list)
+  def user_for(list, link = true)
     user = User.find_by(id: list.user_id) # find_by_id(list.user_id)
     if user
-      link_to "#{user.last_name}, #{user.first_name}", user
+      if link
+        link_to "#{user.last_name}, #{user.first_name}", user
+      else
+        "#{user.last_name}, #{user.first_name}"
+      end
     else
       list.user_id
     end
