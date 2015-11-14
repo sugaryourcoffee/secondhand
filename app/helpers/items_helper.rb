25eb7e189652aea0a33560d91ac9e3a57e1a7d32
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-11-14 17:35:27 +0100

    add highlight for prices greater than alert price in acceptances show view

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -5,9 +5,18 @@ module ItemsHelper
   end
 
   def mark_alert_terms(description)
-    regex = Event.find_by(active: true).alert_terms_regex
+    regex       = Event.find_by(active: true).alert_terms_regex
     alert_terms = description.scan(regex) unless regex.nil?
+
     highlight(description, alert_terms)
+  end
+
+  def mark_alert_value(price)
+    alert_value       = Event.find_by(active: true).alert_value
+    formatted_price   = number_to_currency(price)
+    highlighted_price = formatted_price if price >= alert_value
+
+    highlight(formatted_price, highlighted_price)
   end
 
 end
