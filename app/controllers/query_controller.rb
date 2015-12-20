eda46ab1fb4f2740609c209fa5dbe31e1f2542ac
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-12-20 16:31:05 +0100

    display information and hint how to activate an event if no event is active in the search form

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,19 +1,23 @@
 class QueryController < ApplicationController
 
   def index
+    load_event
     load_items
   end
 
   private
 
+    def load_event
+      @event ||= Event.find_by(active: true)
+    end
+
     def load_items
       keywords = params[:keywords]
-      if keywords.nil? or keywords.strip.empty?
+      if !@event or keywords.nil? or keywords.strip.empty?
         @items = []
       else
-        event = Event.find_by(active: true)
         @items = Item.where("description like ?", "%#{keywords}%")
-                     .joins(:list).where("event_id = ?", event.id)
+                     .joins(:list).where("event_id = ?", @event.id)
       end
     end
 end
--- spec/requests/query_spec.rb
+++ spec/requests/query_spec.rb
@@ -7,41 +7,58 @@ describe "Query" do
   let(:event) { FactoryGirl.create(:active) }
   let(:list)  { FactoryGirl.create(:list, user: user, event: event) }
 
-  before do
-    list.items.create!(item_attributes(item_number: 1, description: "One"))
-    list.items.create!(item_attributes(item_number: 2, description: "Two"))
-    list.items.create!(item_attributes(item_number: 3, description: "Three"))
-    list.items.create!(item_attributes(item_number: 3, description: "One Word"))
-  end
-
-  describe "string" do
-
-    describe "as admin user" do
-      before do
-        sign_in admin
-        visit query_index_path(locale: :en)
-      end
-
-      it "should show all items containing the string" do
-        fill_in "keywords", with: "One"
-        click_button "Search"
-        page.should have_text "One"
-        page.should have_text "One Word"
-      end
+  context "with active event" do
+    before do
+      list.items.create!(item_attributes(item_number: 1, description: "One"))
+      list.items.create!(item_attributes(item_number: 2, description: "Two"))
+      list.items.create!(item_attributes(item_number: 3, description: "Three"))
+      list.items.create!(item_attributes(item_number: 3, 
+                                         description: "One Word"))
     end
 
-    describe "as regular user" do
-      before do
-        sign_in user
-        visit query_index_path(locale: :en)
+    describe "string" do
+
+      describe "as admin user" do
+        before do
+          sign_in admin
+          visit query_index_path(locale: :en)
+        end
+
+        it "should show all items containing the string" do
+          fill_in "keywords", with: "One"
+          click_button "Search"
+          page.should have_text "One"
+          page.should have_text "One Word"
+        end
       end
 
-      it "should return to root path" do
-        page.current_path.should eq root_path(locale: :en)
+      describe "as regular user" do
+        before do
+          sign_in user
+          visit query_index_path(locale: :en)
+        end
+
+        it "should return to root path" do
+          page.current_path.should eq root_path(locale: :en)
+        end
       end
+
     end
-
   end
   
+  context "with no active event" do
+
+    before do
+      sign_in admin
+      event.toggle!(:active)
+      visit query_index_path(locale: :en)
+    end
+
+    it "should indicate no active event" do
+      page.should have_text "Missing active Event"
+    end
+
+  end
+
 end
 
