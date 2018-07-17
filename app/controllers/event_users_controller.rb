0374d9575afaea754c82524e9aac8e5c012d5887
Pierre Sugar <pierre@sugaryourcoffee.de>
2018-07-17 23:29:42 +0200

    update localization for event_-users page

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -2,7 +2,10 @@ class EventUsersController < ApplicationController
 
   def index
     @event = Event.find_by(active: true)
-    @lists = List.where(event_id: @event).where.not(user_id: nil)
+    @lists = List.where(event_id: @event)
+                 .where.not(user_id: nil)
+                 .joins(:user)
+                 .where("users.deactivated = ?", false)
   end
 
 end
