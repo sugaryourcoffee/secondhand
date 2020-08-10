05ee1f66846a8fe374c3ad1f7e7c22eaa4875c24
Pierre Sugar <pierre@sugaryourcoffee.de>
2020-08-10 15:17:32 +0200

    remove rspec/autorun (deprecated), require capybara/rails and set capybara server to :webrick

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,11 +11,16 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
+#  require 'rspec/autorun'
+  require 'capybara/rails'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  # Addition for Capybara 3.33.0 as this uses puma as the default server. We 
+  # are using WEBrick
+  Capybara.server = :webrick
 
   RSpec.configure do |config|
     # ## Mock Framework
