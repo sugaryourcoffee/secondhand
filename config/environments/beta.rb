0f55fd87da7e793275ff25e77bd0f91b80f5dac8
Pierre Sugar <pierre@sugaryourcoffee.de>
2020-08-14 16:35:07 +0200

    replace config_static_assets with config_static_files due to deprecation warning

--- config/environments/beta.rb
+++ config/environments/beta.rb
@@ -20,7 +20,8 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # until 4.1.16: config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
