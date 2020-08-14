255252c004d65ea0308543fd3e3c862c3a2b94f3
Pierre Sugar <pierre@sugaryourcoffee.de>
2020-08-14 16:37:17 +0200

    change from :json to :hybrid due to changes in serializing cookies

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.action_dispatch.cookies_serializer = :json
+# after upgrade to rails 4.2 changed from :json to hybrid
+# :json produced an error:
+# JSON::ParserError (765: unexpected token at
+# {I"session_id:ETI"%05ab6d22f4b16ef34d85820da6eaa357;')
+ 
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
