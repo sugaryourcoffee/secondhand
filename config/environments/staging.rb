7632416f959f1ccb3abfbdeb2047490be2fe10ff
Pierre Sugar <pierre@sugaryourcoffee.de>
2025-01-18 11:18:58 +0100

    remove port from default url, because url forwarding will include port

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # SYC extenstion
   config.action_mailer.perform_deliveries = true
   # SYC extenstion
-  config.action_mailer.default_url_options = { host: "secondhand.sugaryourcoffee.de:8081" }
+  config.action_mailer.default_url_options = { host: "staging-secondhand.sugaryourcoffee.de" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
