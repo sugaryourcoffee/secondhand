39c9c0f1afa5c59e27a28575f7c7df239998331c
Pierre Sugar <pierre@sugaryourcoffee.de>
2025-01-20 11:38:33 +0100

    beautify with rubocob

--- app/mailers/newsletter.rb
+++ app/mailers/newsletter.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Sends newsletter to users that have registered for newsletters
 class Newsletter < ActionMailer::Base
-  default from: "mail@boerse-burgthann.de"
+  default from: 'mail@boerse-burgthann.de'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,7 +12,7 @@ class Newsletter < ActionMailer::Base
   def publish(news, subscribers)
     @description = news.description
 
-    mail to: "mail@boerse-burgthann.de",
+    mail to: 'mail@boerse-burgthann.de',
          bcc: subscribers.join(','),
          subject: news.title
   end
