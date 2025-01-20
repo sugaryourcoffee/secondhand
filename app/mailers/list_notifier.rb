85c61048ff27b19700962c7ed53e938c7e909db5
Pierre Sugar <pierre@sugaryourcoffee.de>
2025-01-20 11:36:28 +0100

    change receiving e-mail to verkaeufe@boerse-burgthann.de

--- app/mailers/list_notifier.rb
+++ app/mailers/list_notifier.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Sends e-mail to seller with labels in pdf- and list in csv-format
 class ListNotifier < ActionMailer::Base
-  default from: "Boerse Burgthann <mail@boerse-burgthann.de>"
+  default from: 'Boerse Burgthann <mail@boerse-burgthann.de>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -12,10 +15,10 @@ class ListNotifier < ActionMailer::Base
     attachments[csv] = File.read(csv)
     pdf = list.labels_pdf(true)
     attachments[pdf] = File.read(pdf)
-    mail to: list.user.email, 
-         bcc: "mail@boerse-burgthann.de",
+    mail to: list.user.email,
+         bcc: 'verkaeufe@boerse-burgthann.de',
          subject: default_i18n_subject(
-           list_number: sprintf("%03d", list.list_number)
+           list_number: format('%03d', list.list_number)
          )
   end
 end
