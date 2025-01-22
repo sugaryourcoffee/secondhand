8ea67d2831267e85bff8e1e4114f4a8a12e7108a
Pierre Sugar <pierre@sugaryourcoffee.de>
2025-01-22 17:19:42 +0100

    add receipts routes and reformat

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,32 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   scope '(:locale)' do
     root                'static_pages#home' # root to: 'static_pages#home'
 
-    get    'signup'  => 'users#new'         # match '/signup',  
-                                            # to: 'users#new'
-    get    'signin'  => 'sessions#new'      # match '/signin',  
-                                            # to: 'sessions#new'
-    delete 'signout' => 'sessions#destroy'  # match '/signout', 
-                                            # to: 'sessions#destroy', 
-                                            # via: :delete
+    get    'signup'  => 'users#new'         # match '/signup',
+    # to: 'users#new'
+    get    'signin'  => 'sessions#new' # match '/signin',
+    # to: 'sessions#new'
+    delete 'signout' => 'sessions#destroy' # match '/signout',
+    # to: 'sessions#destroy',
+    # via: :delete
 
-    get    'about'   => 'static_pages#about' # match '/about',   
-                                             # to: 'static_pages#about'
-    get    'help'    => 'static_pages#help'  # match '/help',    
-                                             # to: 'static_pages#help'
-    get    'contact' => 'static_pages#contact' # match '/contact', 
-                                               # to: 'static_pages#contact'
-    post   'message' => 'static_pages#message' # match '/message', 
-                                               # to: 'static_pages#message'
-    get    'privacy_statement' => 'static_pages#privacy_statement', 
-                                  defaults: { format: 'pdf' }
+    get    'about'   => 'static_pages#about' # match '/about',
+    # to: 'static_pages#about'
+    get    'help'    => 'static_pages#help' # match '/help',
+    # to: 'static_pages#help'
+    get    'contact' => 'static_pages#contact' # match '/contact',
+    # to: 'static_pages#contact'
+    post   'message' => 'static_pages#message' # match '/message',
+    # to: 'static_pages#message'
+    get    'privacy_statement' => 'static_pages#privacy_statement',
+           defaults: { format: 'pdf' }
 
-    get   '/who_registered' => 'users#who_registered', 
-                               defaults: { format: 'atom' }
-    get   '/which_list'     => 'lists#which_list_is_registered_or_closed',
-                               defaults: { format: 'atom' }
+    get   '/who_registered' => 'users#who_registered',
+          defaults: { format: 'atom' }
+    get   '/which_list' => 'lists#which_list_is_registered_or_closed',
+          defaults: { format: 'atom' }
 
     get   'display_terms_of_use' => 'submit_terms_of_use#display'
     get   'accept_terms_of_use'  => 'submit_terms_of_use#accept'
@@ -63,7 +64,7 @@ Rails.application.routes.draw do
 
     resources :counter, only: [:index]
 
-    resources :acceptances, only: [:index, :edit] do
+    resources :acceptances, only: %i[index edit] do
       member do
         get    :edit_list, format: 'js'
         patch  :update_list
@@ -74,7 +75,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :line_items, only: [:create, :destroy]
+    resources :line_items, only: %i[create destroy]
 
     resources :carts do
       collection do
@@ -93,7 +94,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :reversals, only: [:index, :create, :show, :destroy] do
+    resources :reversals, only: %i[index create show destroy] do
       member do
         get :check_out
         get :print
@@ -110,7 +111,7 @@ Rails.application.routes.draw do
       get  'select_lists' => 'importer#lists'
       post 'select_items' => 'importer#select'
       post 'import_items' => 'importer#items'
-     end
+    end
 
     resources :users do
       member do
@@ -125,8 +126,8 @@ Rails.application.routes.draw do
           get :print_labels, defaults: { format: 'pdf' }
           get :send_list
         end
-        resources :items, only: [:index, :new, :create, :show, :edit, :destroy,
-                                 :update]
+        resources :items, only: %i[index new create show edit destroy
+                                   update]
       end
     end
 
@@ -136,7 +137,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :sessions, only: [:new, :create, :destroy]
+    resources :sessions, only: %i[new create destroy]
 
     resources :password_resets
 
@@ -149,6 +150,12 @@ Rails.application.routes.draw do
       end
       collection { get :download_lists_as_pdf }
     end
-  end
 
+    resources :receipts, only: [:index] do
+      member do
+        get :export
+        get :print, defaults: { format: 'pdf' }
+      end
+    end
+  end
 end
